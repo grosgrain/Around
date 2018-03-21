34a32dda7adf8de7f461e585416a4433d46ec3f0
Grosgrain <guyuanguo@gmail.com>
2018-03-20 17:37:26 -0700

    style

--- src/App.js
+++ src/App.js
@@ -7,9 +7,6 @@ class App extends Component {
     return (
       <div className="App">
           <Header/>
-          <p ClassName="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
       </div>
     );
   }
