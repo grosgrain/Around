7ae68fa125da3d70fc4e89b24236059e979d3c81
Grosgrain <guyuanguo@gmail.com>
2018-03-21 17:08:14 -0700

    login form with UI

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react"
 import {Register} from "./register";
+import {Login} from "./login";
 
 export class Main extends Component{
     render() {
         return(
-            <div className="main"><Register/></div>
+            <div className="main">
+                <Login/>
+            </div>
         );
     };
 }
