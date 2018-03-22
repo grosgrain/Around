a97b999eb13f1839662d495026726a9c8be47bcd
Grosgrain <guyuanguo@gmail.com>
2018-03-21 17:25:17 -0700

    add browserRouter

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,18 @@
 import React, {Component} from "react"
 import {Register} from "./register";
 import {Login} from "./login";
+import { Switch, Route } from 'react-router';
 
 export class Main extends Component{
     render() {
         return(
             <div className="main">
-                <Login/>
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route component={Login}/>
+                </Switch>
             </div>
         );
     };
