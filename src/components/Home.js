787f7b17bfb18db20eb83b476b6ba7f18c1da27d
Grosgrain <guyuanguo@gmail.com>
2018-04-05 17:55:19 -0700

    complete google map with real data

--- src/components/Home.js
+++ src/components/Home.js
@@ -101,7 +101,8 @@ export class Home extends React.Component{
                         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
                         loadingElement={<div style={{ height: `100%` }} />}
                         containerElement={<div style={{ height: `400px` }} />}
-                        mapElement={<div style={{ height: `100%` }} />} />
+                        mapElement={<div style={{ height: `100%` }} />}
+                        posts = {this.state.posts}/>
                 </TabPane>
             </Tabs>
         );
